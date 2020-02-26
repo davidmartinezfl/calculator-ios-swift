0d5cdb191d8bac36511a80ecbe8351dbcf7d5997
David Martinez <davidmartinezfl@outlook.com>
2020-02-26 11:19:24 -0600

    [add] mvc architecture and structure of the project

--- calculator-ios-swift/AppDelegate.swift
+++ calculator-ios-swift/AppDelegate.swift
@@ -15,7 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Setup
+        setupView()
+        
         return true
+    }
+    
+    // MARK: - Private methods
+    
+    public func setupView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 
 }
---
+++ calculator-ios-swift/Controller/Home/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  calculator-ios-swift
+//
+//  Created by David Martinez on 26/02/20.
+//  Copyright © 2020 David Martinez. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
