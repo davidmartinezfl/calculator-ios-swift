a35b860dd233fb741379e0ccba10300fb3b11ee9
David Martinez <davidmartinezfl@outlook.com>
2020-02-25 20:14:21 -0600

    Initial Commit

---
+++ calculator-ios-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator-ios-swift
+//
+//  Created by David Martinez on 25/02/20.
+//  Copyright © 2020 David Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
